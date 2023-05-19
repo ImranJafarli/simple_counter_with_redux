0c6ad04c149d080606281d019d00294c50f3d8a4
ImranJafarli <icafarli@gmail.com>
2023-05-19 12:48:40 +0400

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const Counter = (props) => {
+  return (
+    <div>
+      <h2>Counter</h2>
+      <div>
+        <button onClick={() => { props.dispatch({ type: "INCREMENT" }); }}> + </button>
+        <span>{props.count}</span>
+        <button onClick={() => { props.dispatch({ type: "DECREMENT" }); }}> - </button>
+      </div>
+    </div>
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    count: state.count,
+  };
+}
+
+export default connect(mapStateToProps)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Counter from "./Counter";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const initialState = {
+  count: 0,
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1,
+      };
+    case "DECREMENT":
+      if (state.count === 0) {
+        alert("olmaaaaaaaaaaaaaaz")
+        return state;
+      }
+      return {
+        count: state.count - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
